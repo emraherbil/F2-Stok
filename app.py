3e4e37cb517d27edd0d481954cf646af3af64a36
emraherbil <emraherbil@gmail.com>
2026-06-23 17:07:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ st.markdown("""
         iframe[title*="st_keyup"] {
             height: 75px !important;
             min-height: 75px !important;
-            margin-top: -52px !important;
+            margin-top: -55px !important;
             margin-bottom: 0px !important;
             display: block !important;
             position: relative;
