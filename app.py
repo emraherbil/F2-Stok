a52a898cdd23498de0576e7c22d167883c418b4e
emraherbil <emraherbil@gmail.com>
2026-06-24 14:06:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -87,10 +87,6 @@ st.markdown("""
 
         /* 🎯 ARAMA KUTUSU TAŞIYICISI: Selectbox'ların saf boyutu olan 40px'e kilitliyoruz. */
         div[data-testid="column"]:first-child div.element-container:has(iframe[title*="st_keyup"]) {
-        min-height: 70px !important;
-        max-height: 70px !important;
-}
-        div[data-testid="column"]:first-child div.element-container:has(iframe[title*="st_keyup"]) {
             margin-top: -54px !important; /* Senin hizalamayı başardığın o sihirli ölçü */
             overflow: visible !important;
             background: transparent !important; /* Arka planı tamamen yok ettik */
@@ -249,6 +245,10 @@ try:
             v_search = st_keyup(
                 "", 
                 key=f"search_box_{st.session_state.clear_ver}",
+                div[data-testid="column"]:first-child div.element-container:has(iframe[title*="st_keyup"]) {
+        min-height: 70px !important;
+        max-height: 70px !important;
+}
                 placeholder="Yazmaya başlayın...",
                 debounce=300
             )
