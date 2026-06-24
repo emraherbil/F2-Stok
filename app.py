14ab6ba49753cc9b6b6b2938b3ac8e3bd0800b0d
emraherbil <emraherbil@gmail.com>
2026-06-24 15:58:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ st.markdown("""
         /* 🎯 TEMİZLE BUTONU DİKEY HİZALAMASI */
         /* Selectbox ve Input etiketlerinin (Label) kapladığı 28px'lik boşluk kadar butonu aşağı indirir */
         div[data-testid="column"] .stButton {
-            margin-top: 28px !important;
+            margin-top: 20px !important;
         }
 
         /* 🎯 TEMİZLE BUTONU TASARIMI VE BOYUTU */
