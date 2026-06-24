def84117a6138e052e4106df5dd707c8b2267584
emraherbil <emraherbil@gmail.com>
2026-06-24 14:53:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ st.markdown("""
 
         /* 🎯 ARAMA KUTUSU TAŞIYICISI: Selectbox'ların saf boyutu olan 40px'e kilitliyoruz. */
         div[data-testid="column"]:first-child div.element-container:has(iframe[title*="st_keyup"]) {
-            margin-top: -54px !important; /* Senin hizalamayı başardığın o sihirli ölçü */
+            margin-top: 0px !important; /* Senin hizalamayı başardığın o sihirli ölçü */
             overflow: visible !important;
             background: transparent !important; /* Arka planı tamamen yok ettik */
             z-index: 50 !important;
