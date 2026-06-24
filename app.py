a5f6350c715dbb231ba177d7d8716e7d6c9897b9
emraherbil <emraherbil@gmail.com>
2026-06-24 14:13:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -240,14 +240,21 @@ try:
         if current_grup not in grup_ops:
             st.session_state.q_grup = "Tümü"
 
-        with col1:
-            st.markdown('<span class="sabit-arama-etiketi">📝 Ürün Ara</span>', unsafe_allow_html=True)
-            v_search = st_keyup(
-                "", 
-                key=f"search_box_{st.session_state.clear_ver}",
-                placeholder="Yazmaya başlayın...",
-                debounce=300
-            )
+        wwith col1:
+    st.markdown(
+        '<span class="sabit-arama-etiketi">📝 Ürün Ara</span>',
+        unsafe_allow_html=True
+    )
+
+    search_placeholder = st.empty()
+
+    with search_placeholder:
+        v_search = st_keyup(
+            "",
+            key=f"search_box_{st.session_state.clear_ver}",
+            placeholder="Yazmaya başlayın...",
+            debounce=300
+        )
 
         with col2:
             v_marka = st.selectbox("🏷️ Marka", marka_ops, key="q_marka")
