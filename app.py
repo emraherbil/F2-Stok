be701c11577162c37b39a67541a84a4aad7071e6
emraherbil <emraherbil@gmail.com>
2026-08-30 17:05:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ st.markdown("""
 
         /* 🎯 CHECKBOX BLOK HİZALAMASI: İki checkbox'ı birden input hizasına çekmek için */
         .checkbox-align-box {
-            margin-top: 0px !important;
+            margin-top: 6px !important;
         }
 
         /* Temizle Butonu Özel Hizalaması */
